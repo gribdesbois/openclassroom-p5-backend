52a6af54be8e3b3b6cfb7ac20b79060c7db6b014
gribdesbois <gribdesbois@gmail.com>
2022-02-15 14:32:05 +0100

    changed  Acces Control Allow Origin

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 
+
 const cameraRoutes = require('./routes/camera');
 const teddyRoutes = require('./routes/teddy');
 const furnitureRoutes = require('./routes/furniture');
@@ -8,7 +9,7 @@ const furnitureRoutes = require('./routes/furniture');
 const app = express();
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://inspiring-newton-9b905b.netlify.app/');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
