d0aa61b4f32bfb743aac9cbcfd335f855f295b2d
gribdesbois <gribdesbois@gmail.com>
2022-02-15 14:55:15 +0100

    issues with cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors')
 
 
 const cameraRoutes = require('./routes/camera');
@@ -7,13 +8,18 @@ const teddyRoutes = require('./routes/teddy');
 const furnitureRoutes = require('./routes/furniture');
 
 const app = express();
+const corsOptions = {
+  'Access-Control-Allow-Origin':
+    'https://inspiring-newton-9b905b.netlify.app',
+}
+app.use(cors(corsOptions))
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://inspiring-newton-9b905b.netlify.app/');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
-});
+}); */
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(express.static('images'));
